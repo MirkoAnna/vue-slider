f7f790dcb5f56dc02edba8bde28d41f110a62e47
MirkoAnna <82913293+MirkoAnna@users.noreply.github.com>
2022-02-10 15:51:37 +0100

    Alcune correzioni + visualizzzo su schermo thumb tramite v-for

--- js/main.js
+++ js/main.js
@@ -2,18 +2,35 @@ const vue = new Vue (
     {
         el: '#app',
         data: {
-            
             active: 0,
-            items: {
 
-                url:['img/01.jpg','img/02.jpg','img/03.jpg','img/04.jpg','img/05.jpg',],
-                name:['Svezia','Svizzera','Gran Bretagna','Germania','Paradise',],
-                text: ['Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                        'Lorem ipsum',
-                        'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-                        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-                        'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,']
-            }               
+            items: [
+                {
+                    url:'img/01.jpg',
+                    name:'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                },
+                {
+                    url:'img/02.jpg',
+                    name:'Svizzera',
+                    text:'Lorem ipsum'
+                },
+                {
+                    url:'img/03.jpg',
+                    name:'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+                },
+                {
+                    url:'img/04.jpg',
+                    name:'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                },
+                {
+                    url:'img/05.jpg',
+                    name:'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                }
+            ]
         }
     }
 )
